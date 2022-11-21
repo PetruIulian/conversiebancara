58b00f351addbd14d9526a66eba4ac86370d5938
PetruIulian <petruiulian01@gmail.com>
2022-11-21 20:09:01 +0200

    actions

--- lib/main.dart
+++ lib/main.dart
@@ -28,52 +28,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  final _controller = TextEditingController();
-  String convertOutput = "";
+  final TextEditingController _controller = TextEditingController();
+  String convertOutput = '';
   double pretEur = 4.8;
-  double? input = 0.0;
+  double? input = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text(widget.title)),
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.network("https://media.hotnews.ro/media_server1/image-2021-06-18-24869240-0-lei-romanesti.jpg",
-                fit: BoxFit.cover),
-            Padding(
-              padding: const EdgeInsets.all(28.0),
-              child: TextField(
-                controller: _controller,
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                decoration: InputDecoration(
-                    labelText: "Enter number of euros",
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25)
-                    )
+      appBar: AppBar(
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Column(
+        children: <Widget>[
+          Image.network(
+            'https://media.hotnews.ro/media_server1/image-2021-06-18-24869240-0-lei-romanesti.jpg',
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(28),
+            child: TextField(
+              controller: _controller,
+              keyboardType: const TextInputType.numberWithOptions(decimal: true),
+              decoration: InputDecoration(
+                labelText: 'Enter number of euros',
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
                 ),
-                onChanged: (String value){
-                  setState(() {
-                    input = double.tryParse(value);
-                  });
-                },
               ),
+              onChanged: (String value) {
+                setState(() {
+                  input = double.tryParse(value);
+                });
+              },
             ),
-            ElevatedButton(onPressed:(){
+          ),
+          ElevatedButton(
+            onPressed: () {
               setState(() {
-                convertOutput = "${double.parse((pretEur * input!).toStringAsFixed(2))}";
+                convertOutput =
+                    '${double.parse((pretEur * input!).toStringAsFixed(2))}';
               });
-            }, child: Text("Convert!")),
-            Text("${convertOutput} RON",
-              style: TextStyle(
-                fontSize: 48,
-              ),)
-          ],
-        ) // This trailing comma makes auto-formatting nicer for build methods.
+            },
+            child: const Text('Convert!'),
+          ),
+          Text(
+            '$convertOutput RON',
+            style: const TextStyle(
+              fontSize: 48,
+            ),
+          )
+        ],
+      ) // This trailing comma makes auto-formatting nicer for build methods.
+      ,
     );
   }
 }
